cb57b00085f82b1fe70fd275ebfb88502f47d52b
Ezequiel <ezequieluris06@gmail.com>
2026-07-28 21:56:17 -0300

    Fix script android, remover Jugar.jsx y revertir cambios de linter en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,26 +9,26 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import ReactGA from "react-ga4";
 import * as Sentry from "@sentry/react";
-import { registerSW } from "virtual:pwa-register";
+import { registerSW } from 'virtual:pwa-register'
 ReactGA.initialize("G-03YHGYYXEL");
 
 Sentry.init({
   dsn: "https://664075e5a2cf584cd7c109287a91921a@o4511718208569344.ingest.us.sentry.io/4511718222069760",
-  dataCollection: {},
+  dataCollection: {
+  }
 });
 
 const updateSW = registerSW({
   onNeedRefresh() {
-    console.log("Nueva versión disponible");
-    updateSW();
+    console.log('Nueva versión disponible, refrescar la página')
   },
   onOfflineReady() {
-    console.log("La app está lista offline");
+    console.log('La app está lista para usarse offline')
   },
-});
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 );
